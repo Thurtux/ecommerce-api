1597d0d5b3deaa92ba0911fce93a6551130aca36
Thurtux <arthusantoslopes91@gmail.com>
2025-02-18 16:49:33 -0300

    Correção: Firebase via variável de ambiente

--- src/config/firebaseConfig.js
+++ src/config/firebaseConfig.js
@@ -1,5 +1,5 @@
 const admin = require("firebase-admin");
-const serviceAccount = require("./firebase-service-account.json"); // Certifique-se de que este arquivo existe!
+const serviceAccount = JSON.parse(process.env.FIREBASE_CREDENTIALS);
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
