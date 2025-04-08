7172ab95be4428b9da7c92596b60d3bca0b859c1
Thurtux <arthursantoslopes91@gmail.com>
2025-04-08 13:21:25 -0300

    Update allowedOrigins

--- src/server.ts
+++ src/server.ts
@@ -12,7 +12,7 @@
 
   const app = express();
 
-  const allowedOrigins = ["http://localhost:5173", "https://seu-site-em-producao.com"];
+  const allowedOrigins = ["http://localhost:5173", "https://adminecommerc.netlify.app/"];
 
   app.use(
     cors({
